4f208d49f55ad8437b80f4a01c645da02947e74f
YongjieX <xue515953749@gmail.com>
2023-12-12 20:47:54 -0600

    revision

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
 import "dotenv/config";
+import routes from "./src/routes/index.js";
 
 const app = express();
 
@@ -12,20 +13,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// app.use("/api/v1", routes);
+app.use("/api/v1", routes);
 
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log("Mongodb connected");
-    server.listen(port, () => {
-        console.log(`Server is listening on port ${port}`);
-    });
+  console.log("Mongodb connected");
+  server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
 }).catch((err) => {
-    console.log({ err });
-    process.exit(1);
+  console.log({ err });
+  process.exit(1);
 });
 
 //test
